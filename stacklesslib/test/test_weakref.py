71804e04e1c885b3b686fba6eb5217c66502e0b1
Kristján Valur Jónsson <sweskman@gmail.com>
2014-01-06 20:53:33 +0000

    Adding a stackelsslib.weakref module which has support for WeakMethods. WeakMethod is backported from python 3, WeakMethodProxy is new design which uses that.

---
+++ stacklesslib/test/test_weakref.py
@@ -0,0 +1,213 @@
+# test_weakref
+
+import weakref
+import unittest
+import stacklesslib.weakref
+
+class TestClass(object):
+    def method(self, *args):
+        return args
+
+def func():
+    pass
+
+class TestWeakMethod(unittest.TestCase):
+    def setUp(self):
+        self.c = TestClass()
+
+    def test_create(self):
+        r = stacklesslib.weakref.WeakMethod(TestClass().method)
+
+    def test_weak(self):
+        r = stacklesslib.weakref.WeakMethod(TestClass().method)
+        self.assertEqual(r(), None)
+
+    def test_strong(self):
+        r = stacklesslib.weakref.WeakMethod(self.c.method)
+        self.assertEqual(r(), self.c.method)
+        del self.c
+        self.assertEqual(r(), None)
+
+    def test_fail(self):
+        self.assertRaises(TypeError, stacklesslib.weakref.WeakMethod, func)
+
+    def test_eq(self):
+        m = self.c.method
+        r = stacklesslib.weakref.WeakMethod(m)
+        self.assertEqual(m, r())
+
+    def test_repr(self):
+        m = self.c.method
+        r = stacklesslib.weakref.WeakMethod(m)
+        self.assertEqual(repr(m), repr(r()))
+
+    def test_ref(self):
+        m = self.c.method
+        r = stacklesslib.weakref.WeakMethod(m)
+        weakref.ref(r)
+
+
+class TestProxy(unittest.TestCase):
+    """Test some properties of callable proxies that we want to emulate"""
+    def setUp(self):
+        def dude():
+            pass
+        self.dude = dude
+
+    def test_callback(self):
+        foo = [None]
+        def cb(arg):
+            foo[0] = arg
+        p = weakref.proxy(self.dude, cb)
+        del self.dude
+        self.assertEqual(id(foo[0]), id(p))
+
+    def test_hash(self):
+        p = weakref.proxy(self.dude)
+        self.assertRaises(TypeError, hash, p)
+        del self.dude
+        self.assertRaises(TypeError, hash, p)
+
+    def test_cmp(self):
+        def cb1(arg): pass
+        def cb2(arg): pass
+        p1 = weakref.proxy(self.dude, cb1)
+        p2 = weakref.proxy(self.dude, cb2)
+        self.assertNotEqual(id(p1), id(p2))
+        self.assertEqual(p1, p2)
+        del self.dude
+        def cmp(a, b): return a == b
+        self.assertRaises(weakref.ReferenceError, cmp, p1, p2)
+
+
+class TestWeakMethodProxy(unittest.TestCase):
+    def setUp(self):
+        self.c = TestClass()
+
+    def test_create(self):
+        r = stacklesslib.weakref.WeakMethodProxy(self.c.method)
+
+    def test_weak(self):
+        r = stacklesslib.weakref.WeakMethodProxy(TestClass().method)
+        self.assertRaises(weakref.ReferenceError, r)
+
+    def test_strong(self):
+        r = stacklesslib.weakref.WeakMethodProxy(self.c.method)
+        result = r(1,2,3)
+        self.assertEqual(result, (1, 2, 3))
+        del self.c
+        self.assertRaises(weakref.ReferenceError, r)
+
+    def test_strong_callback(self):
+        foo = [None]
+        def cb(arg):
+            foo[0] = arg
+        r = stacklesslib.weakref.WeakMethodProxy(self.c.method, callback=cb)
+        result = r(1,2,3)
+        self.assertEqual(result, (1, 2, 3))
+        self.assertEqual(foo[0], None)
+        del self.c
+        self.assertRaises(weakref.ReferenceError, r)
+        self.assertEqual(id(foo[0]), id(r))
+
+    def test_fallback(self):
+        def fb(*arg):
+            return arg+arg
+        r = stacklesslib.weakref.WeakMethodProxy(self.c.method, fallback=fb)
+        result = r(1,2,3)
+        self.assertEqual(result, (1, 2, 3))
+        del self.c
+        result = r(1,2,3)
+        self.assertEqual(result, (1, 2, 3)*2)
+
+    def test_hash(self):
+        r = stacklesslib.weakref.WeakMethodProxy(self.c.method)
+        self.assertRaises(TypeError, hash, r)
+        del self.c
+        self.assertRaises(TypeError, hash, r)
+
+    def test_repr(self):
+        r = stacklesslib.weakref.WeakMethodProxy(self.c.method)
+        inner = repr(self.c.method)
+        outer = repr(r)
+        self.assertTrue(inner in outer)
+        del self.c
+        outer = repr(r)
+        self.assertFalse(inner in outer)
+        self.assertTrue("None" in outer)
+
+    def test_cmp(self):
+        def cb1(arg): pass
+        def cb2(arg): pass
+        p1 = stacklesslib.weakref.WeakMethodProxy(self.c.method)
+        p2 = stacklesslib.weakref.WeakMethodProxy(self.c.method)
+        self.assertNotEqual(id(p1), id(p2))
+        self.assertEqual(p1, p2)
+        del self.c
+        def cmp(a, b): return a == b
+        self.assertRaises(weakref.ReferenceError, cmp, p1, p2)
+
+class TestRef(unittest.TestCase):
+    def test_object(self):
+        o = TestClass()
+        p1 = weakref.ref(o)
+        p2 = stacklesslib.weakref.ref(o)
+        self.assertEqual(p1, p2)
+
+    def test_object_cb(self):
+        def cb1(arg): pass
+        o = TestClass()
+        p1 = weakref.ref(o, cb1)
+        p2 = stacklesslib.weakref.ref(o, cb1)
+        self.assertEqual(p1, p2)
+
+    def test_func(self):
+        def o(): pass
+        p1 = weakref.ref(o)
+        p2 = stacklesslib.weakref.ref(o)
+        self.assertEqual(p1, p2)
+
+    def test_func(self):
+        c = TestClass()
+        o = c.method
+        p1 = weakref.ref(o)
+        p2 = stacklesslib.weakref.ref(o)
+        self.assertNotEqual(p1, p2)
+        p3 = stacklesslib.weakref.WeakMethod(o)
+        self.assertNotEqual(p1, p3)
+
+class TestProxy(unittest.TestCase):
+    def test_object(self):
+        o = TestClass()
+        p1 = weakref.proxy(o)
+        p2 = stacklesslib.weakref.proxy(o)
+        self.assertEqual(p1, p2)
+
+    def test_object_cb(self):
+        def cb1(arg): pass
+        o = TestClass()
+        p1 = weakref.proxy(o, cb1)
+        p2 = stacklesslib.weakref.proxy(o, cb1)
+        self.assertEqual(p1, p2)
+
+    def test_func(self):
+        def o(): pass
+        p1 = weakref.proxy(o)
+        p2 = stacklesslib.weakref.proxy(o)
+        self.assertEqual(p1, p2)
+
+    def test_func(self):
+        c = TestClass()
+        o = c.method
+        p1 = weakref.proxy(o)
+        p2 = stacklesslib.weakref.proxy(o)
+        self.assertNotEqual(p1, p2)
+        p3 = stacklesslib.weakref.WeakMethodProxy(o)
+        self.assertNotEqual(p1, p3)
+
+
+
+
+
+
+
---
+++ stacklesslib/weakref.py
@@ -0,0 +1,122 @@
+# weakref
+# contains weak method and weak method proxy helper classes to manage callbacks
+# The WeakMethod is ported from Python 3.  The WeakMethodProxy works similarly to
+# weakref.weakcallableproxy
+
+from __future__ import absolute_import # To get real weakref module
+import weakref
+
+try:
+    from weakref import WeakMethod
+except ImportError:
+
+    # backport of the WeakMethod class from python 3.x
+    class WeakMethod(weakref.ref):
+        """
+        A custom `weakref.ref` subclass which simulates a weak reference to
+        a bound method, working around the lifetime problem of bound methods.
+        """
+
+        __slots__ = "_func_ref", "_meth_type", "_alive", "__weakref__"
+
+        def __new__(cls, meth, callback=None):
+            try:
+                obj = meth.__self__
+                func = meth.__func__
+            except AttributeError:
+                raise TypeError("argument should be a bound method, not {}"
+                                .format(type(meth)))
+            def _cb(arg):
+                # The self-weakref trick is needed to avoid creating a reference
+                # cycle.
+                self = self_wr()
+                if self._alive:
+                    self._alive = False
+                    if callback is not None:
+                        callback(self)
+            self = weakref.ref.__new__(cls, obj, _cb)
+            self._func_ref = weakref.ref(func, _cb)
+            self._meth_type = type(meth)
+            self._alive = True
+            self_wr = weakref.ref(self)
+            return self
+
+        def __call__(self):
+            obj = super(WeakMethod, self).__call__()
+            func = self._func_ref()
+            if obj is None or func is None:
+                return None
+            return self._meth_type(func, obj, type(obj))
+
+        def __eq__(self, other):
+            if isinstance(other, WeakMethod):
+                if not self._alive or not other._alive:
+                    return self is other
+                return ref.__eq__(self, other) and self._func_ref == other._func_ref
+            return False
+
+        def __ne__(self, other):
+            if isinstance(other, WeakMethod):
+                if not self._alive or not other._alive:
+                    return self is not other
+                return ref.__ne__(self, other) or self._func_ref != other._func_ref
+            return True
+
+        __hash__ = weakref.ref.__hash__
+
+
+class WeakMethodProxy(object):
+    """
+    A weak proxy to a weak method.  Similar to weakref.proxy() but
+    works with bound methods.  has a "fallback" argument, which replaces
+    the target as a delegate when the target goes away.
+    """
+    def __init__(self, meth, callback=None, fallback=None):
+        """
+        Create a weak proxy to a bound method.  "callback" is called
+        with this proxy as an argument when the underlying WeakMethod
+        object goes stale.  "fallback" is then used as a delegate instead.
+        """
+        if callback:
+            self_wr = weakref.ref(self)
+            def _cb(arg):
+                # The self-weakref trick is needed to avoid creating a reference
+                # cycle.
+                callback(self)
+            self.weak_method = WeakMethod(meth, callback=_cb)
+        else:
+            self.weak_method = WeakMethod(meth)
+        self.fallback = fallback
+
+    def __call__(self, *args, **kwds):
+        method = self.weak_method()
+        if method:
+            return method(*args, **kwds)
+        if self.fallback:
+            return self.fallback(*args, **kwds)
+        raise weakref.ReferenceError("weakly-referenced object no longer exists")
+    __hash__ = None
+
+    def __repr__(self):
+        return "<WeakMethodProxy at %d to %r>"%(id(self), self.weak_method())
+
+    def __eq__(self, other):
+        if isinstance(other, WeakMethodProxy):
+            a, b = self.weak_method(), other.weak_method()
+            if a is None or b is None:
+                raise weakref.ReferenceError("weakly-referenced object no longer exists")
+            return a == b
+
+def ref(obj, callback=None):
+    """Create a weak reference, returning a WeakMethod if appropriate"""
+    try:
+        return WeakMethod(obj, callback)
+    except TypeError:
+        return weakref.ref(obj, callback)
+
+def proxy(obj, callback=None, fallback=None):
+    """Create a proxy, returning a WeakMethodProxy if appropriate"""
+    try:
+        return WeakMethodProxy(obj, callback, fallback)
+    except TypeError:
+        return weakref.proxy(obj, callback)
